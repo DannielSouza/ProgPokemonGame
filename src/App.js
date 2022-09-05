50c5928204d458dba27d4c0da4af95b8866dc8fb
DannielSouza <104663666+DannielSouza@users.noreply.github.com>
2022-09-05 18:44:34 -0300

    responsive update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Screen from './Components/Screen';
+import mobile from './assets/phone.png'
+import rotate from './assets/rotate.png'
 
 function App() {
   return (
     <div className="App">
       <Screen/>
+      <section className='mobileView'>
+        <div className='iconContainer'>
+          <img className='phoneIcon' src={mobile} alt="celular" />
+          <img className='rotateIcon' src={rotate} alt="rotacionar" />
+          <div className='mobileMessage'>
+            <p>Por favor, vire o celular.</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

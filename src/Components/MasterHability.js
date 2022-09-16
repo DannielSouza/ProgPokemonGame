c65b3ed60d426001e86d3d920b2a8c4cd36b374d
DannielSouza <104663666+DannielSouza@users.noreply.github.com>
2022-09-16 15:02:42 -0300

    masterhability alert added

--- src/Components/MasterHability.js
+++ src/Components/MasterHability.js
@@ -2,9 +2,12 @@ import React from 'react'
 import style from '../Styles/MasterHability.module.css'
 
 const MasterHability = ({player, setPlayer, setBossStats}) => {
-  const masterButon = React.useRef()
 
+  
+  const masterButon = React.useRef()
+  
   function masterHability(){
+    window.alert(`Vendo seu esforço, o ${player.name} se inspirou, recuperando parte de seu HP e reduzindo o dano do inimigo.`)
     setPlayer((before)=>({
       ...before, hp: before.hp + 2100, atack: before.atack + 40
     }))
